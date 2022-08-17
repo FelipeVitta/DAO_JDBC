bf51f93fcc0326d8036dde21457c7a029431cada
FelipeVitta <feliperiku@gmail.com>
2022-08-17 18:14:18 -0300

    primeiro commit

--- projeto-JDBC/src/Application/App.java
+++ projeto-JDBC/src/Application/App.java
@@ -1,6 +1,10 @@
+package Application;
 
 import db.DB;
+import model_entities.Department;
+
 import java.sql.Statement;
+import java.security.spec.ECFieldF2m;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,6 +12,8 @@ import java.sql.SQLException;
 public class App {
     
     public static void main(String[] args) {
+
+       
         
      
     }
